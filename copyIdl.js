b8c4ea68e353c7b380f2ea17f58f96fae12177e2
Matt Lim <mlimcollege@gmail.com>
2021-10-22 15:45:40 -0700

    Run node copyIdl

--- copyIdl.js
+++ copyIdl.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
-const idl = require('./target/idl/solana_nextjs.json');
+const idl = require('./target/idl/solana_nextjs_counter.json');
 
 fs.writeFileSync('./app/src/idl.json', JSON.stringify(idl));
